2c99e9523aba33bfadaea1c62ff77cd11897c49a
Pedrosmp <pedrosmp1@al.insper.edu.br>
2024-01-22 09:26:33 -0300

    final

--- ep2.py
+++ ep2.py
@@ -1,3 +1,4 @@
+import random
 def define_posicoes(dados):
     l=[]
     l.append([dados['linha'],dados['coluna']])
@@ -68,4 +69,73 @@ def posicao_valida(dados,frota):
             if coord in pos_desejada:
                 return False
     return True
+
+
+def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+    '''
+    tabuleiro_jogador: tabuleiro do jogador
+    tabuleiro_oponente: tabuleiro do oponente
+    Função monta uma string com a representação dos tabuleiros do jogador e do oponente.
+    O tabuleiro do jogador é representado por um tabuleiro com as posições dos navios.
+    O tabuleiro do oponente é representado por um tabuleiro com as posições que o jogador já atirou.
+    '''
+
+    texto = ''
+    texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+    texto += '_______________________________      _______________________________\n'
+
+    for linha in range(len(tabuleiro_jogador)):
+        jogador_info = '  '.join([str(item)
+                                  for item in tabuleiro_jogador[linha]])
+        oponente_info = '  '.join(
+            [info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+        texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+    texto += '_______________________________      _______________________________\n'
+    return texto
+
+
+def gerando_frota_automaticamente():
+    '''
+    Função gera uma frota de navios de forma aleatória.
+    '''
+    quantidades = {
+        "submarino": {
+            "quantidade": 4,
+            "tamanho": 1
+        },
+        "destroyer": {
+            "quantidade": 3,
+            "tamanho": 2
+        },
+        "navio-tanque": {
+            "quantidade": 2,
+            "tamanho": 3
+        },
+        "porta-aviões": {
+            "quantidade": 1,
+            "tamanho": 4
+        }
+    }
+
+    frota = []
+
+    for nome_navio, info in quantidades.items():
+        for _ in range(info["quantidade"]):
+            dados_de_posicionamento = {
+                "tamanho": info["tamanho"],
+            }
+            dados_de_posicionamento["orientacao"] = random.choice(
+                ["vertical", "horizontal"])
+            dados_de_posicionamento["linha"] = random.randint(0, 9)
+            dados_de_posicionamento["coluna"] = random.randint(0, 9)
+
+            while not posicao_valida(dados_de_posicionamento, frota):
+                dados_de_posicionamento["orientacao"] = random.choice(
+                    ["vertical", "horizontal"])
+                dados_de_posicionamento["linha"] = random.randint(0, 9)
+                dados_de_posicionamento["coluna"] = random.randint(0, 9)
+
+            preenche_frota(dados_de_posicionamento, nome_navio, frota)
+
+    return frota
 
